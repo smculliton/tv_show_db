aabc340f7dfb52d8f9b2fd03cba59f871d94cef8
Sean Culliton <smculliton@gmail.com>
2022-11-03 20:37:39 -0600

    Add user create page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
-  
+  def show 
+    @user = User.find(params[:id])
+  end
+
+  def new 
+  end
+
+  def create
+    user = User.new(user_params)
+
+    user.save
+    redirect_to "/users/#{user.id}"
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
 
+  resources :users
+
   get '/shows', to: 'shows#index' 
   get '/shows/new', to: 'shows#new'
   post '/shows', to: 'shows#create'
